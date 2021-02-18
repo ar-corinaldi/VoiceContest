dffc218a12b4a45f46a247d1144e200a73303dcf
Daniel Babativa <da.babativa@uniandes.edu.co>
2021-02-18 09:28:25 -0500

    DA: Fixed path

--- back/processor.py
+++ back/processor.py
@@ -4,16 +4,16 @@ from os import listdir
 from os.path import isfile, join
 
 
-onlyfiles = [f for f in listdir('unprocessed/') if isfile(join('unprocessed/', f))]
+onlyfiles = [f for f in listdir('/home/estudiante/VoiceContest/back/unprocessed/') if isfile(join('/home/estudiante/VoiceContest/back/unprocessed/', f))]
 
 print(onlyfiles)
 
 for f in onlyfiles:
     f_no_extension = f.split(".")[0]
-    string_command = "ffmpeg -i original/{} processed/{}.mp3"
+    string_command = "ffmpeg -i /home/estudiante/VoiceContest/back/original/{} /home/estudiante/VoiceContest/back/processed/{}.mp3"
     print(string_command, "String")
-    command = f"ffmpeg -i unprocessed/{f} processed/{f_no_extension}.mp3"
+    command = f"ffmpeg -i /home/estudiante/VoiceContest/back/unprocessed/{f} /home/estudiante/VoiceContest/back/processed/{f_no_extension}.mp3"
     print(command)
     os.system(command)
-    delete_command = f"rm unprocessed/{f}"
+    delete_command = f"rm /home/estudiante/VoiceContest/back/unprocessed/{f}"
     os.system(delete_command)
