d4d74f9a40ceafdc29d5679b83fae3f205cf256e
Daniel Babativa <da.babativa@uniandes.edu.co>
2021-02-18 19:26:14 -0500

    DA: Emails work, now time for updater

--- back/app.py
+++ back/app.py
@@ -372,24 +372,18 @@ class ResourseOneVoice(Resource):
 
 class ResourceVoiceUpdater(Resource):
     def get(self):
-        # voices = Voice.query.filter_by(state="En proceso")
-        # print(voices)
-        # orderedListVoices = posts_voice_schema.dump(voices.items)
-        # print(result)
-        # processed_files = [f for f in listdir('/home/estudiante/VoiceContest/back/processed/') if isfile(join('/home/estudiante/VoiceContest/back/processed/', f))]
-        # for voice in orderedListVoices:
-        #     processed_filename = voice.transformed_voice_file_path
-        #     if processed_filename in processed_files:
-        #         voice.state = "Procesada"
+        voices = Voice.query.filter_by(state="En proceso")
+        print(voices)
+        orderedListVoices = posts_voice_schema.dump(voices.items)
+        print(result)
+        processed_files = [f for f in listdir('/home/estudiante/VoiceContest/back/processed/') if isfile(join('/home/estudiante/VoiceContest/back/processed/', f))]
+        for voice in orderedListVoices:
+            processed_filename = voice.transformed_voice_file_path
+            if processed_filename in processed_files:
+                voice.state = "Procesada"
                 
-        # db.session.commit()
-        # msg = Message( 
-        #             'Hello', 
-        #             sender ='voice.contest.cloud@gmail.com', 
-        #             recipients = ['da.babativa@uniandes.edu.co', 'da.babativa@outlook.com', 'babat00@outlook.com'] 
-        #         ) 
-        # msg.body = 'Hello Flask message sent from Flask-Mail'
-        # mail.send(msg) 
+        db.session.commit()
+      
         s = smtplib.SMTP('smtp.gmail.com', 587) 
         s.starttls()
         s.login("voice.contest.cloud@gmail.com", "Cl0ud123") 
