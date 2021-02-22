c0dcf4b8026d630bec44ea53e2e99690d47a1612
Daniel Babativa <da.babativa@uniandes.edu.co>
2021-02-21 22:14:20 -0500

    DA: reverse

--- back/processor.py
+++ back/processor.py
@@ -4,13 +4,13 @@ from os import listdir
 from os.path import isfile, join
 
 
-onlyfiles = [f for f in listdir('/home/estudiante/VoiceContest/back/unprocessed/') if isfile(join('/home/estudiante/VoiceContest/back/unprocessed/', f))]
+onlyfiles = [f for f in listdir('/home/estudiante/VoiceContest/back/processed/') if isfile(join('/home/estudiante/VoiceContest/back/processed/', f))]
 
 print(onlyfiles)
 
 for f in onlyfiles:
     f_no_extension = f.split(".")[0]
-    command = f"ffmpeg -i /home/estudiante/VoiceContest/back/unprocessed/{f} /home/estudiante/VoiceContest/back/processed/{f_no_extension}.wav"
+    command = f"ffmpeg -i /home/estudiante/VoiceContest/back/processed/{f} /home/estudiante/VoiceContest/back/unprocessed/{f_no_extension}.wav"
     os.system(command)
-    delete_command = f"rm /home/estudiante/VoiceContest/back/unprocessed/{f}"
+    delete_command = f"rm /home/estudiante/VoiceContest/back/processed/{f}"
     os.system(delete_command)
--- back/updater.py
+++ back/updater.py
@@ -3,5 +3,6 @@ import requests
 try:
     r =requests.get('http://172.24.98.143:5000/update-processed')
     print(r)
+    print("Voices updates and mail sent")
 except:
     print("Error")
