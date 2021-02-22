5764eaf634be22b53a5a42f21ebd7af574679695
Daniel Babativa <da.babativa@uniandes.edu.co>
2021-02-21 22:03:01 -0500

    DA: Done

--- back/updater.py
+++ back/updater.py
@@ -1,7 +1,7 @@
 import requests
 
 try:
-    r =requests.get('http://172.24.98.143/update-processed')
+    r =requests.get('http://172.24.98.143:5000/update-processed')
     print(r)
 except:
     print("Error")
