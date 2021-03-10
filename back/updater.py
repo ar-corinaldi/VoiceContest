712b0dfe8580ba991ac7409e2e6ae5fb83bef5f5
Daniel Babativa <da.babativa@uniandes.edu.co>
2021-03-10 16:34:22 -0500

    DA: Updater points to url

--- back/updater.py
+++ back/updater.py
@@ -2,7 +2,7 @@ import requests
 
 try:
     print("pre")
-    r =requests.get('http://172.24.98.143/update-processed')
+    r =requests.get('http://18.223.156.195/update-processed')
     print("post")
     print(r)
 except:
