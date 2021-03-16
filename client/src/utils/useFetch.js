64186522b653e5fb077fdf0c19c0700edce522e5
Daniel Babativa <da.babativa@uniandes.edu.co>
2021-03-16 14:04:22 -0500

    DA: Build

--- client/src/utils/useFetch.js
+++ client/src/utils/useFetch.js
@@ -1,4 +1,4 @@
-export async function doFetch(url = "", method = "GET", body, token) {
+export async function doFetch(url = "/", method = "GET", body, token) {
   let URL = `${process.env.REACT_APP_URL_ENDPOINTS_TEST}${url}`;
   console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV === "production") {
